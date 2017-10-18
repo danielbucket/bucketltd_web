88a35f03df066d3010e7ca3b19238646dec725dc
Daniel Bucket <danieljbucket@gmail.com>
2017-10-18 16:22:09 -0600

    in a previous attempt to make a personal site, I had made the header modular and so I simply copied it from that project and used it here. I only needed to make minor adjustments

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+
+    <header className="navbar-header"
+            style={require('./Navbar.css')}
+            >
+      <img className="bucket-logo"
+        			src={require("./images/bucket-icon-yellow-b.png")}
+        			/>
+      <h1 className="page-title">
+        <span className="yellow"><span className="the-b">B</span>ucket</span>
+        <span className="un">Un</span>
+        <span className="yellow">Limited</span>
+      </h1>
+
+    </header>
+
+  );
+};
+
+export default Navbar;
